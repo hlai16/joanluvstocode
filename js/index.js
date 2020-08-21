a89d644212356dc8e38af199c676c19a594c8408
Gloria <gloriajoanlai@gmail.com>
2020-08-21 17:24:12 -0400

    added about me :smile:

--- js/index.js
+++ js/index.js
@@ -5,5 +5,23 @@ $(document).ready(function () {
         dropRadius: 20,
         perturbance: 0.02,
         interactive: true
-    });   
+    });
+    scrollFadeIn();
+});
+
+// credit to Staffan Adolfsson on CodePen, learn more about jqery while googling this code. I probably don't need animat.css now.
+const scrollFadeIn = $(document).on('scroll', function () {
+    const pageTop = $(document).scrollTop();
+    const pageBottom = pageTop + $(window).height();
+    let about = $('.about');
+
+    for (let i = 0; i < about.length; i++) {
+        about = about[i];
+
+        if ($(about).position().top < pageBottom) {
+            $(about).addClass('visible');
+        } else {
+            $(about).removeClass('visible');
+        }
+    }
 });
