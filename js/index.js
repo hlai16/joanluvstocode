645b971bd0bbb8108db1b2a7b53d9d1aa8511899
Gloria <gloriajoanlai@gmail.com>
2020-08-21 00:30:44 -0400

    first commit

---
+++ js/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $('.rippleEffect').ripples({
+        imageUrl: null,
+        resolution: 256,
+        dropRadius: 20,
+        perturbance: 0.02,
+        interactive: true
+    });   
+});
