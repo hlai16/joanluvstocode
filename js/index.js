3110d8c26975ccfffb2487ba93bd76f960d025bc
Gloria <gloriajoanlai@gmail.com>
2020-08-26 04:40:13 -0400

    some assignments added

--- js/index.js
+++ js/index.js
@@ -1,3 +1,20 @@
+assignmentInfoArray = [
+    sortingHat = {
+        description: 'An app for Harry Potter fan to be sorted into one of the Hogwart houses. This app used the Harry Potter API.',
+        link: `https://hlai16.github.io/sortingHatCeremonyByJoanLai/`,
+    },
+    poster = {
+        description: 'A movie poster created using photoshop',
+        link: `img/condor-heroes.jpg`,
+    },
+    chart = {
+        description: 'An article on cultured meat written by me. I used js chart plugins to show some of the data mentioned in the article.',
+        link: `#`,
+    },
+]
+
+
+
 const joansOffice = {};
 
 // credit to Staffan Adolfsson on CodePen, learn more about jqery while googling this code. I probably don't need animat.css now.
@@ -50,13 +67,67 @@ joansOffice.hamburgerOn = () => {
     })
 }
 
-    
+// SLICK PLUGIN
+joansOffice.slick = $(document).ready(function () {
+    $('.myAssignments').slick({
+        accessibility: true,
+        adaptiveHeight: false,
+        arrows: true,
+        prevArrow: '<button type="button" data-role="none" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" data-role="none" class="slick-next">Next</button>',
+        autoplay: false,
+        cssEase: 'ease',
+        customPaging: function (slider, i) {
+            return '<button type="button" data-role="none">' + (i + 1) + '</button>';
+        },
+        dots: true,
+        dotsClass: 'slick-dots',
+        draggable: true,
+        easing: 'linear',
+        edgeFriction: 0.35,
+        infinite: true,
+        mobileFirst: true,
+        pauseOnHover: true,
+        pauseOnFocus: true,
+        pauseOnDotsHover: false,
+        respondTo: 'mobile',
+        speed: 1000,
+        zIndex: 10
+    });
+});
 
+joansOffice.slideHoverShowsInfo = () => {
+    $('.slide1').on('click', () => {
+        $('.assignmentInfo').html(
+            `<div class="slideCover animate__animated animate__pulse">
+                <p>${assignmentInfoArray[0].description}</p>
+                <p>Link: <a href="${assignmentInfoArray[0].link}">Click Here</a></p>
+                </div>`
+        )
+    })
+    $('.slide2').on('click', () => {
+        $('.assignmentInfo').html(
+            `<div class="slideCover animate__animated animate__pulse">
+                <p>${assignmentInfoArray[1].description}</p>
+                <p>Link: <a href="${assignmentInfoArray[1].link}">Click Here</a></p>
+                </div>`
+        )
+    })
+    $('.slide3').on('click', () => {
+        $('.assignmentInfo').html(
+            `<div class="slideCover animate__animated animate__pulse">
+                <p>${assignmentInfoArray[2].description}</p>
+                <p>Link: <a href="${assignmentInfoArray[2].link}">Click Here</a></p>
+                </div>`
+        )
+    })   
+}
 
 //init
 
-joansOffice.init = () => { 
+joansOffice.init = () => {
     joansOffice.hamburgerOn();
+    joansOffice.slideHoverShowsInfo();
 }
 
 //doc ready
